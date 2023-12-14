8e36b7a028a1495af87f420b279d0eeca92ca991
H.G. Shoemaker <hgshoemaker@proton.me>
2023-12-14 13:10:53 -0600

    added inverter for CS adjusted spi mode to 1

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 ESP32DMASPI::Slave slave;
 
-static const uint32_t BUFFER_SIZE = 8192;
+static const uint32_t BUFFER_SIZE = 3120;
 uint8_t* spi_slave_tx_buf;
 uint8_t* spi_slave_rx_buf;
 
@@ -14,6 +14,38 @@ void set_buffer() {
     memset(spi_slave_rx_buf, 0, BUFFER_SIZE);
 }
 
+constexpr uint8_t CORE_TASK_SPI_SLAVE {0};
+constexpr uint8_t CORE_TASK_PROCESS_BUFFER {0};
+
+static TaskHandle_t task_handle_wait_spi = 0;
+static TaskHandle_t task_handle_process_buffer = 0;
+
+void task_wait_spi(void* pvParameters) {
+    while (1) {
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+
+        slave.wait(spi_slave_rx_buf, spi_slave_tx_buf, BUFFER_SIZE);
+
+        xTaskNotifyGive(task_handle_process_buffer);
+    }
+}
+
+void task_process_buffer(void* pvParameters) {
+    while (1) {
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+
+        // show received data
+        for (size_t i = 0; i < BUFFER_SIZE; ++i) {
+            printf("%d ", spi_slave_rx_buf[i]);
+        }
+        printf("\n");
+
+        slave.pop();
+
+        xTaskNotifyGive(task_handle_wait_spi);
+    }
+}
+
 void setup() {
     Serial.begin(115200);
 
@@ -22,6 +54,7 @@ void setup() {
     spi_slave_rx_buf = slave.allocDMABuffer(BUFFER_SIZE);
 
     set_buffer();
+
     delay(5000);
 
     // slave device configuration
@@ -32,25 +65,18 @@ void setup() {
     // note: the default pins are different depending on the board
     // please refer to README Section "SPI Buses and SPI Pins" for more details
     slave.begin();  // HSPI
+
+    xTaskCreatePinnedToCore(task_wait_spi, "task_wait_spi", 2048, NULL, 2, &task_handle_wait_spi, CORE_TASK_SPI_SLAVE);
+    xTaskNotifyGive(task_handle_wait_spi);
+
+    xTaskCreatePinnedToCore(
+        task_process_buffer,
+        "task_process_buffer",
+        2048,
+        NULL,
+        2,
+        &task_handle_process_buffer,
+        CORE_TASK_PROCESS_BUFFER);
 }
 
-void loop() {
-    // if there is no transaction in queue, add transaction
-    if (slave.remained() == 0) {
-        slave.queue(spi_slave_rx_buf, spi_slave_tx_buf, BUFFER_SIZE);
-    }
-
-    // if transaction has completed from master,
-    // available() returns size of results of transaction,
-    // and buffer is automatically updated
-
-    while (slave.available()) {
-        // show received data
-        for (size_t i = 0; i < BUFFER_SIZE; ++i) {
-            printf("%d ", spi_slave_rx_buf[i]);
-        }
-        printf("\n");
-
-        slave.pop();
-    }
-}
+void loop() {}
